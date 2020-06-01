25fe354d9f6d5d64b4906d5a15902e38aa551a33
Georgi Velev <georgi.a.velev@gmail.com>
2020-06-01 19:47:16 +0300

    Initial commit - set up project structure

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # smarthome-backend
 Server application for a university IoT project "Smart Home"
+
+## Used technologies
+1. Mosquitto MQTT broker
+2. MySQL RDBMS
+3. Spring Framework & Spring Boot
+4. Docker
+
+## Short description
+This is the back-end server application for the "Smart Home" university project for "Distributed Embedded Systems" discipline.
+
+### Functionalities:
+- Room lights and temperature monitoring
+- Temperature control via commands based on the temperature sensor readings
+    - commands issued to relays controlling a fan and a heater
+- Lights control via commands based on the lights sensor readings
+    - commands issued to relays the room lights
+    
+### Hardware:
+- 2x NodeMCU (ESP8266) modules
+- 4x AC relays (10A/250V)
+- 1x DHT11 temperature and humidity sensor
+- 1x photoresistor as a light sensor
+
+## How-to
+1. Download and install Docker -> [link](https://hub.docker.com/editions/community/docker-ce-desktop-windows/)
+2. Execute ``docker-compose up`` in the root directory of the project
+3. Run the Spring Boot appliaction - ``./mvnw spring-boot:run``
+
---
+++ src/main/java/com/smarthome/backend/config/MqttConfig.java
@@ -0,0 +1,97 @@
+package com.smarthome.backend.config;
+
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.datatypes.MqttClientIdentifier;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * MQTT client configuration.
+ */
+@Configuration
+public class MqttConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MqttConfig.class);
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${smarthome.mqtt.server.address}")
+    private String serverAddress;
+
+    @Value("${smarthome.mqtt.server.port}")
+    private Integer serverPort;
+
+    @Bean
+    public Mqtt3Client mqttClient() throws ExecutionException, InterruptedException {
+        Mqtt3AsyncClient mqtt3AsyncClient = MqttClient.builder()
+                .useMqttVersion3()
+                .addConnectedListener(listener ->
+                        LOGGER.info("MQTT client connected: mqttVersion='{}', server='{}:{}', state='{}', clientId='{}'",
+                                listener.getClientConfig().getMqttVersion().name(),
+                                listener.getClientConfig().getServerHost(),
+                                listener.getClientConfig().getServerPort(),
+                                listener.getClientConfig().getState().name(),
+                                listener.getClientConfig().getClientIdentifier().get())
+                )
+                .addDisconnectedListener(listener ->
+                        LOGGER.info("MQTT client disconnected: source='{}', mqttVersion='{}', server='{}:{}', state='{}', clientId='{}', cause='{}'",
+                                listener.getSource().name(),
+                                listener.getClientConfig().getMqttVersion().name(),
+                                listener.getClientConfig().getServerHost(),
+                                listener.getClientConfig().getServerPort(),
+                                listener.getClientConfig().getState().name(),
+                                listener.getClientConfig().getClientIdentifier().get(),
+                                listener.getCause())
+                )
+                .identifier(MqttClientIdentifier.of(applicationName))
+                .serverHost(serverAddress)
+                .serverPort(serverPort)
+                .automaticReconnect()
+                .initialDelay(5, TimeUnit.SECONDS)
+                .maxDelay(10, TimeUnit.SECONDS)
+                .applyAutomaticReconnect()
+                .buildAsync();
+
+        mqttClientConnect(mqtt3AsyncClient);
+
+        return mqtt3AsyncClient;
+    }
+
+    /**
+     * Establishes MQTT client connectivity to the MQTT broker.
+     *
+     * @param mqtt3AsyncClient MQTT async client
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    private void mqttClientConnect(Mqtt3AsyncClient mqtt3AsyncClient) throws ExecutionException, InterruptedException {
+        while (!mqtt3AsyncClient.getState().isConnected()) {
+            mqtt3AsyncClient.connectWith()
+                    .keepAlive(600)
+                    .send()
+                    .whenComplete((connAck, throwable) -> {
+                        if (throwable != null) {
+                            // Handle failure:
+                            LOGGER.error("Connection to MQTT broker has failed", throwable);
+                            mqtt3AsyncClient.disconnect();
+                        } else {
+                            // Setup subscriptions to broker's $SYS topics:
+                            LOGGER.info("MQTT client connection: state='{}', session='{}', type='{}', returnCode='{}'",
+                                    mqtt3AsyncClient.getState().name(), connAck.isSessionPresent(), connAck.getType(), connAck.getReturnCode().name());
+                        }
+                    })
+                    .get(); // Call get() to ensure client is connected when this method exits
+        }
+    }
+
+
+}
